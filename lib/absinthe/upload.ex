6230c968c2d55a925132b19d91104ee6e29dc6a7
shavit <shavit@users.noreply.github.com>
2020-11-12 21:21:45 -0500

    safe decode variables

--- lib/absinthe/upload.ex
+++ lib/absinthe/upload.ex
@@ -6,26 +6,27 @@ defmodule Absinthe.Upload do
   def init(conn), do: conn
 
   def call(%{body_params: %{"map" => mvars}} = conn, _) do
-    map_json = Jason.decode!(mvars)
-    drop_keys = Map.keys(map_json) ++ ["operations", "map"]
+    with {:ok, map_json} <- Jason.decode(mvars) do
+      query = get_operations(conn)
+      upload_params = get_upload_params(conn, map_json)
+      drop_keys = Map.keys(map_json) ++ ["operations", "map"]
 
-    upload_params = get_upload_params(conn, map_json)
-    query = get_operations(conn)
-
-    conn
-    |> Map.update(:params, conn.params, fn x ->
-      x
-      |> Map.put("query", query)
-      |> Map.drop(drop_keys)
-      |> Enum.into(upload_params)
-    end)
+      conn
+      |> Map.update!(:params, fn x ->
+        x
+        |> Map.put("query", query)
+        |> Map.drop(drop_keys)
+        |> Enum.into(upload_params)
+      end)
+    else
+      _ -> conn
+    end
   end
 
   def call(conn, _), do: conn
 
   defp get_upload_params(conn, json) do
-    json
-    |> Enum.reduce(%{}, fn {a, [b]}, acc ->
+    Enum.reduce(json, %{}, fn {a, [b]}, acc ->
       key = b |> String.split(".") |> List.last()
       value = conn |> Map.get(:params) |> Map.get(a)
 
@@ -34,7 +35,10 @@ defmodule Absinthe.Upload do
   end
 
   defp get_operations(%{:body_params => %{"operations" => j}}) do
-    j |> Jason.decode!() |> Map.get("query") |> String.replace("\n", "")
+    case Jason.decode(j) do
+      {:ok, m} -> m |> Map.get("query") |> String.replace("\n", "")
+      {:error, _reason} -> nil
+    end
   end
 
   defp get_operations(_conn), do: nil
--- test/absinthe/upload_test.exs
+++ test/absinthe/upload_test.exs
@@ -6,4 +6,40 @@ defmodule Absinthe.UploadTest do
   test "init/1 pass the conn" do
     assert Upload.init(%{}) == %{}
   end
+
+  test "conn/2 transforms request parameters" do
+    params_ = %{
+      "0" => %{
+        content_type: "text/rtf",
+        filename: "San Francisco.rtf",
+        path: "/tmp/plug-1605/multipart-1605259564-858222931410900-3"
+      },
+      "map" => "{\"0\":[\"variables.attachment.0\"]}",
+      "operations" =>
+        "{\"query\":\"mutation DemoUpload($attachment: Upload) {\\n  demoUpload(attachment: $attachment)\\n}\",\"variables\":{\"attachment\":[null]},\"operationName\":\"DemoUpload\"}"
+    }
+
+    opts = []
+
+    conn = Upload.call(%{body_params: params_, params: params_}, opts)
+    assert %{params: params} = conn
+
+    assert params["query"] ==
+             "mutation DemoUpload($attachment: Upload) {  demoUpload(attachment: $attachment)}"
+
+    assert params["0"] == %{
+             content_type: "text/rtf",
+             filename: "San Francisco.rtf",
+             path: "/tmp/plug-1605/multipart-1605259564-858222931410900-3"
+           }
+
+    assert Map.get(conn, "map") == nil
+    assert Map.get(conn, "operations") == nil
+  end
+
+  test "conn/2 ignore parse errors" do
+    assert %{params: %{}} == Upload.call(%{params: %{}}, [])
+    assert %{body_params: %{}} == Upload.call(%{body_params: %{}}, [])
+    assert %{:body_params => nil, "operations" => nil} == Upload.call(%{:body_params => nil, "operations" => nil}, [])
+  end
 end
